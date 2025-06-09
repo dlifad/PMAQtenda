c3795e1dffcdf52d10fbc4116e08120958910994
Zainur Fadli <zainurfadli2005@gmail.com>
2025-06-09 15:46:33 +0700

    memperbarui perubahan status penyewaan

--- app/Models/Jadwal.php
+++ app/Models/Jadwal.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
 
 class Jadwal extends Model
 {
@@ -14,7 +15,7 @@ class Jadwal extends Model
 
     // Konstanta untuk status jadwal
     public const STATUS_TERJADWAL = 'terjadwal';
-    public const STATUS_TERPASANG = 'terpasang'; 
+    public const STATUS_TERPASANG = 'terpasang';
     public const STATUS_TERBONGKAR = 'terbongkar';
 
     protected $fillable = [
@@ -26,8 +27,34 @@ class Jadwal extends Model
         'status',
     ];
 
+    /**
+     * Relasi dengan model Penyewaan.
+     */
     public function penyewaan()
     {
         return $this->belongsTo(Penyewaan::class, 'id_penyewaan', 'id_penyewaan');
+    }
+
+    /**
+     * Boot the model.
+     * Di sinilah kita akan meletakkan logika event.
+     */
+    protected static function booted()
+    {
+        static::updated(function ($jadwal) {
+            // Cek apakah kolom 'status' yang berubah
+            if ($jadwal->isDirty('status')) {
+                // Jika status jadwal menjadi 'terpasang'
+                if ($jadwal->status === self::STATUS_TERPASANG) {
+                    // Update status penyewaan menjadi 'berlangsung'
+                    $jadwal->penyewaan->update(['status' => Penyewaan::STATUS_BERLANGSUNG]);
+                } 
+                // Jika status jadwal menjadi 'terbongkar'
+                elseif ($jadwal->status === self::STATUS_TERBONGKAR) {
+                    // Update status penyewaan menjadi 'selesai'
+                    $jadwal->penyewaan->update(['status' => Penyewaan::STATUS_SELESAI]);
+                }
+            }
+        });
     }
 }
